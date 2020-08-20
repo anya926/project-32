ef63882e7c80c9fae8e21bfdc61bb385ba79a710
anya926 <68541162+anya926@users.noreply.github.com>
2020-08-20 18:03:45 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,39 @@
+class Box{
+    constructor(x,y,width,height){
+        var options = {
+            'restitution':0.8,
+            'friction':0.001,
+            'density':0.001
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.visibility = 255;
+
+        World.add(world, this.body);
+    }
+    display(){
+        if(this.body.speed<3){
+            var pos = this.body.position;
+            var angle = this.body.angle;
+            push();
+            translate(pos.x,pos.y);
+            rotate(angle);
+            rectMode(CENTER);
+            rect(0, 0, this.width, this.height);
+            pop();
+          }    
+          else{
+            World.remove(world, this.body);
+            push();
+            score= score+5;
+            if(score>=200){
+                score= 200;
+            }
+            //tint(255,125);
+            //rect(this.body.position.x,this.body.position.y,this.width,this.height);
+            this.visibility = this.visibility-5;
+            pop();
+          }
+    }
+};
